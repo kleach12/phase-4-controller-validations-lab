b8445ed8f75643168c5fbd660247651ea15e6b84
Kale Leach <kleach1212@gmail.com>
2022-09-20 09:40:08 -0700

    commit tree fd555ffe38adbbb0865bbd91908b563d12c672e1

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,12 +6,24 @@ class PostsController < ApplicationController
     render json: post
   end
 
+  def create 
+    post = Post.create(post_params)
+      if post.valid?
+        render json: post, status: :created
+      else
+        render json: {errors: post.errors}, status: :unprocessable_entity
+      end
+  end
+
+
   def update
     post = Post.find(params[:id])
-
     post.update(post_params)
-
-    render json: post
+    if post.valid?
+      render json: post
+    else
+      render json: {errors: post.errors}, status: :unprocessable_entity
+    end
   end
 
   private
